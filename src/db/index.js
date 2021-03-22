8cc06647dd8cee2d38693b27a471d2f8315f1642
Audrey <audreyuu@gmail.com>
2021-03-21 22:44:30 -0400

    update ssl

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: true
 });
 
 client
   .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+  .catch(err => console.log(`Error connecting to Postgres server:\n${err}`));
 
 module.exports = client;
